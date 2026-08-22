a465278e58cd7b3c64b65b28833f3d8f4e0fbf71
Hoang Nghi <hoangnghinguyen17@gmail.com>
2026-08-22 18:12:38 +0700

    logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,20 @@
-import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>App đầu tiền của bố</View>
+    <View style={style.View}>
+      <Text style={style.Text}>App đầu tiền của bố, ahahahahahaha</Text>
+    </View>
   );
 }
 
+const style = StyleSheet.create({
+  View:{
+    backgroundColor: '#000'
+  },
+  Text:{
+    color: '#fff',
+    fontSize:50,
+    display:'flex',
+  }
+})
